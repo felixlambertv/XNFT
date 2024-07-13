cc683475875bdc9befd2b443f95667a7bd0344b3
Felix Lambert <felix@itsavirus.com>
2024-07-14 03:32:21 +0700

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "./tasks";
+
+const SEPOLIA_API_KEY = vars.get("SEPOLIA_API_KEY");
+const ETHERS_API_KEY = vars.get("ETHERS_API_KEY");
+const DEPLOYER_PRIVATE_KEY = vars.get("DEPLOYER_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_API_KEY}`,
+      accounts: [
+        DEPLOYER_PRIVATE_KEY, //deployer
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERS_API_KEY,
+  },
 };
 
 export default config;
